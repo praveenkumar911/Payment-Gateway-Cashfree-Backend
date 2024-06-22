b490ce1d87175eca2ec622e11783f60fa21d5106
Praveen Kumar Palaboyina <113330666+praveenkumar911@users.noreply.github.com>
2024-06-22 22:34:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Cashfree } = require('cashfree-pg');
-const Cors = require('cors');
+const cors = require('cors');
+
 const app = express();
 const port = 5000;
 
-app.use(Cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configure Cashfree SDK with your credentials and environment for production
+// Configure Cashfree SDK with your production credentials
 Cashfree.XClientId = "704574427dcfe9140b0ed50016475407"; // Production Client ID
 Cashfree.XClientSecret = "cfsk_ma_prod_0f839fa28421fe634bcd3dd34c04440c_ef3f80d7"; // Production Client Secret
 Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION; // Set to PRODUCTION for live environment
 
-// Single API endpoint for creating an order and displaying the payment interface
+// Route to initiate payment
 app.post('/initiate_payment', async (req, res) => {
   try {
     const request = {
@@ -24,20 +25,19 @@ app.post('/initiate_payment', async (req, res) => {
       customer_details: {
         customer_id: req.body.customer_id,
         customer_phone: req.body.customer_phone,
-        customer_email: req.body.customer_email // Assuming email is also provided
+        customer_email: req.body.customer_email
       },
       order_meta: {
-        return_url: `https://payment-gateway-cashfree-frontend.vercel.app/payment_success?order_id=${req.body.order_id}` // Local URL for testing
+        return_url: `https://payment-gateway-cashfree-frontend.vercel.app/payment_success?order_id=${req.body.order_id}`
       }
     };
 
-    // Create order
     const response = await Cashfree.PGCreateOrder("2022-09-01", request);
     console.log('Order Created successfully:', response.data);
 
     res.json({
-      payment_session_id: response.data.payment_session_id, // Return session ID for frontend
-      payment_url: `https://payment-gateway-cashfree-frontend.vercel.app/payment_page?payment_session_id=${response.data.payment_session_id}` // Local frontend payment page URL
+      payment_session_id: response.data.payment_session_id,
+      payment_url: `https://payment-gateway-cashfree-frontend.vercel.app/payment_page?payment_session_id=${response.data.payment_session_id}`
     });
   } catch (error) {
     console.error('Error:', error.response ? error.response.data.message : error.message);
@@ -45,12 +45,9 @@ app.post('/initiate_payment', async (req, res) => {
   }
 });
 
-// Add new route to welcome users to the gateway
+// Route to handle greeting
 app.get('/', (req, res) => {
-  res.send('Hi, welcome to the gateway');
-});
-app.get('/ok', (req, res) => {
-  res.send('ok');
+  res.send('Hi! Welcome to the payment gateway.');
 });
 
 app.listen(port, () => {
